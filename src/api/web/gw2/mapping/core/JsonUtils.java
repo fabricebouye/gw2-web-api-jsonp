c2fb3c5cb4877b89360e7c33f6cf37537ce33c5b
Fabrice Bouyé <fabriceb@spc.int>
2015-07-16 12:08:40 +1100

    Added support for ids string generation. Added support for retrieving optional nullable values.

--- src/api/web/gw2/mapping/core/JsonUtils.java
+++ src/api/web/gw2/mapping/core/JsonUtils.java
@@ -10,6 +10,7 @@ package api.web.gw2.mapping.core;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -20,6 +21,7 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import javax.json.Json;
 import javax.json.JsonArray;
+import javax.json.JsonNumber;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonString;
@@ -27,7 +29,7 @@ import javax.json.JsonValue;
 
 /**
  * JSR 353 JSON-P utility class.
-* <br>This class provides core utility methods that are used throughout this factory implementation.
+ * <br>This class provides core utility methods that are used throughout this factory implementation.
  * @author Fabrice Bouyé
  */
 public enum JsonUtils {
@@ -35,10 +37,49 @@ public enum JsonUtils {
     INSTANCE;
 
     /**
+     * Converts JSON string into Java strings.
+     */
+    public static final Function<JsonString, String> STRING_CONVERTER = JsonString::getString;
+
+    /**
+     * Converts JSON number into Java integers.
+     */
+    public static final Function<JsonNumber, Integer> INTEGER_CONVERTER = JsonNumber::intValue;
+    /**
+     * Converts JSON number into Java longs.
+     */
+    public static final Function<JsonNumber, Long> LONG_CONVERTER = JsonNumber::longValue;
+
+    /**
+     * Converts a list of ids to a parameter string.
+     * @param ids The source ids.
+     * @return A {@code String} instance, never {@code null}.
+     * <br>If no ids are provided, the value {@code "all"} is returned.
+     */
+    public static String idsToParameter(final int... ids) {
+        final String result = (ids.length == 0) ? "all" : Arrays.stream(ids) // NOI18N.
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(",")); // NOI18N.
+        return result;
+    }
+
+    /**
+     * Converts a list of ids to a parameter string.
+     * @param ids The source ids.
+     * @return A {@code String} instance, never {@code null}.
+     * <br>If no ids are provided, the value {@code "all"} is returned.
+     */
+    public static String idsToParameter(final String... ids) {
+        final String result = (ids.length == 0) ? "all" : Arrays.stream(ids) // NOI18N.
+                .collect(Collectors.joining(",")); // NOI18N.
+        return result;
+    }
+
+    /**
      * Gets a JSON array from the provided URL.
      * @param basecode The source URL.
      * @return A {@code JSonArray} instance, never {@code null}.
-     * @throws NullPointerException If (@code basecode} is {@code null}.    
+     * @throws NullPointerException If {@code basecode} is {@code null}.    
      * @throws IOException In case of IO error.
      */
     public static JsonArray asJsonArray(final String basecode) throws NullPointerException, IOException {
@@ -53,7 +94,7 @@ public enum JsonUtils {
      * Gets a JSON object from the provided URL.
      * @param basecode The source URL.
      * @return A {@code JsonObject} instance, never {@code null}.
-     * @throws NullPointerException If (@code basecode} is {@code null}.    
+     * @throws NullPointerException If {@code basecode} is {@code null}.    
      * @throws IOException In case of IO error.
      */
     public static JsonObject asJsonObject(final String basecode) throws NullPointerException, IOException {
@@ -81,70 +122,76 @@ public enum JsonUtils {
     }
 
     /**
-     * Converts an array of JSON objects into a list of Java objects.
+     * Converts a JSON array into a list of Java objects.
+     * @param <S> The type of the values within the JSON array.
      * @param <V> The output type.
      * @param basecode The source URL.
+     * @param sourceClass The type of the values within the JSON array.    
      * @param converter Converts a value of type {@code JsonObject} -> a value of type {@code V}.
      * @return A non-modifiable {@code List<V>} instance, never {@code null}.
-     * @throws NullPointerException If either (@code basecode} or (@code converter} is {@code null}.
+     * @throws NullPointerException If either {@code basecode}, {@code sourceClass} or {@code converter} is {@code null}.
      * @throws IOException In case of IO error.
      */
-    public static <V> List<V> listFromJsonObjectArray(final String basecode, final Function<JsonObject, V> converter) throws NullPointerException, IOException {
+    public static <S extends JsonValue, V> List<V> listFromJsonArray(final String basecode, final Class<S> sourceClass, final Function<S, V> converter) throws NullPointerException, IOException {
         Objects.requireNonNull(basecode);
+        Objects.requireNonNull(sourceClass);
         Objects.requireNonNull(converter);
         final JsonArray jsonArray = asJsonArray(basecode);
-        final List<V> result = jsonArrayToCollection(jsonArray, JsonObject.class, converter, Collectors.toList());
+        final List<V> result = (jsonArray == null || jsonArray.isEmpty()) ? Collections.EMPTY_LIST : jsonArrayToCollection(jsonArray, sourceClass, converter, Collectors.toList());
         return Collections.unmodifiableList(result);
     }
 
     /**
-     * Converts an array of JSON strings into a list of Java objects.
+     * Converts a JSON array into a list of Java objects.
+     * @param <S> The type of the values within the JSON array.
      * @param <V> The output type.
-     * @param basecode The source URL.
+     * @param jsonArray The source array, may be {@code null}.
+     * @param sourceClass The type of the values within the JSON array.    
      * @param converter Converts a value of type {@code JsonString} -> a value of type {@code V}.
      * @return A non-modifiable {@code List<V>} instance, never {@code null}.
-     * @throws NullPointerException If either (@code basecode} or (@code converter} is {@code null}.
-     * @throws IOException In case of IO error.
+     * @throws NullPointerException If either {@code sourceClass} or {@code converter} is {@code null}.
      */
-    public static <V> List<V> listFromJsonStringArray(final String basecode, final Function<JsonString, V> converter) throws NullPointerException, IOException {
-        Objects.requireNonNull(basecode);
+    public static <S extends JsonValue, V> List<V> listFromJsonArray(final JsonArray jsonArray, final Class<S> sourceClass, final Function<S, V> converter) throws NullPointerException {
+        Objects.requireNonNull(sourceClass);
         Objects.requireNonNull(converter);
-        final JsonArray jsonArray = asJsonArray(basecode);
-        final List<V> result = jsonArrayToCollection(jsonArray, JsonString.class, converter, Collectors.toList());
+        final List<V> result = (jsonArray == null || jsonArray.isEmpty()) ? Collections.EMPTY_LIST : jsonArrayToCollection(jsonArray, sourceClass, converter, Collectors.toList());
         return Collections.unmodifiableList(result);
     }
 
     /**
-     * Converts an array of JSON objects into a set of Java objects.
+     * Converts a JSON array into a set of Java objects.
+     * @param <S> The type of the values within the JSON array.
      * @param <V> The output type.
      * @param basecode The source URL.
+     * @param sourceClass The type of the values within the JSON array.    
      * @param converter Converts a value of type {@code JsonObject} -> a value of type {@code V}.
      * @return A non-modifiable {@code Set<V>} instance, never {@code null}.
-     * @throws NullPointerException If either (@code basecode} or (@code converter} is {@code null}.
+     * @throws NullPointerException If either {@code basecode}, {@code sourceClass} or {@code converter} is {@code null}.
      * @throws IOException In case of IO error.
      */
-    public static <V> Set<V> setFromJsonOpjectArray(final String basecode, final Function<JsonObject, V> converter) throws NullPointerException, IOException {
+    public static <S extends JsonValue, V> Set<V> setFromJsonArray(final String basecode, final Class<S> sourceClass, final Function<S, V> converter) throws NullPointerException, IOException {
         Objects.requireNonNull(basecode);
+        Objects.requireNonNull(sourceClass);
         Objects.requireNonNull(converter);
         final JsonArray jsonArray = asJsonArray(basecode);
-        final Set<V> result = jsonArrayToCollection(jsonArray, JsonObject.class, converter, Collectors.toSet());
+        final Set<V> result = (jsonArray == null || jsonArray.isEmpty()) ? Collections.EMPTY_SET : jsonArrayToCollection(jsonArray, sourceClass, converter, Collectors.toSet());
         return Collections.unmodifiableSet(result);
     }
 
     /**
-     * Converts an array of JSON strings into a set of Java objects.
+     * Converts a JSON array into a set of Java objects.
+     * @param <S> The type of the values within the JSON array.
      * @param <V> The output type.
-     * @param basecode The source URL.
+     * @param jsonArray The source array, may be {@code null}.
+     * @param sourceClass The type of the values within the JSON array.    
      * @param converter Converts a value of type {@code JsonString} -> a value of type {@code V}.
      * @return A non-modifiable {@code Set<V>} instance, never {@code null}.
-     * @throws NullPointerException If either (@code basecode} or (@code converter} is {@code null}.
-     * @throws IOException In case of IO error.
+     * @throws NullPointerException If either {@code sourceClass} or {@code converter} is {@code null}.
      */
-    public static <V> Set<V> setFromJsonStringArray(final String basecode, final Function<JsonString, V> converter) throws NullPointerException, IOException {
-        Objects.requireNonNull(basecode);
+    public static <S extends JsonValue, V> Set<V> setFromJsonArray(final JsonArray jsonArray, final Class<S> sourceClass, final Function<S, V> converter) throws NullPointerException {
+        Objects.requireNonNull(sourceClass);
         Objects.requireNonNull(converter);
-        final JsonArray jsonArray = asJsonArray(basecode);
-        final Set<V> result = jsonArrayToCollection(jsonArray, JsonString.class, converter, Collectors.toSet());
+        final Set<V> result = (jsonArray == null || jsonArray.isEmpty()) ? Collections.EMPTY_SET : jsonArrayToCollection(jsonArray, sourceClass, converter, Collectors.toSet());
         return Collections.unmodifiableSet(result);
     }
 
@@ -170,5 +217,101 @@ public enum JsonUtils {
                 .map(converter)
                 .collect(collector);
         return result;
+    }
+
+    /**
+     * Gets a Java integer from a key that may be missing or {@code NULL}.
+     * @param source The source JSON object.
+     * @param key The key.
+     * @param defaultValue The default value.
+     * @return An {@code int} instance, may be {@code defaultValue} if {@code key} is {@code NULL} or absent from {@code source}.
+     * @throws NullPointerException If either {@code source} or {@code key} is {@code null}.
+     */
+    public int nullOrMissingInt(final JsonObject source, final String key, final int defaultValue) throws NullPointerException {
+        Objects.requireNonNull(source);
+        Objects.requireNonNull(key);
+        return nullOrMissingValue(source, source::getInt, key, defaultValue);
+    }
+
+    /**
+     * Gets a Java {@code String} from a key that may be missing or {@code NULL}.
+     * @param source The source JSON object.
+     * @param key The key.
+     * @return A {@code String} instance, may be {@code null} if {@code key} is {@code NULL} or absent from {@code source}.
+     * @throws NullPointerException If either {@code source} or {@code key} is {@code null}.
+     */
+    public String nullOrMissingString(final JsonObject source, final String key) throws NullPointerException {
+        Objects.requireNonNull(source);
+        Objects.requireNonNull(key);
+        return nullOrMissingValue(source, source::getString, key, null);
+    }
+
+    /**
+     * Gets a JSON string from a key that may be missing or {@code NULL}.
+     * @param source The source JSON object.
+     * @param key The key.
+     * @return A {@code JsonString} instance, may be {@code null} if {@code key} is {@code NULL} or absent from {@code source}.
+     * @throws NullPointerException If either {@code source} or {@code key} is {@code null}.
+     */
+    public JsonString nullOrMissingJsonString(final JsonObject source, final String key) throws NullPointerException {
+        Objects.requireNonNull(source);
+        Objects.requireNonNull(key);
+        return nullOrMissingValue(source, source::getJsonString, key, null);
+    }
+
+    /**
+     * Gets a JSON object from a key that may be missing or {@code NULL}.
+     * @param source The source JSON object.
+     * @param key The key.
+     * @return A {@code JsonObject} instance, may be {@code null} if {@code key} is {@code NULL} or absent from {@code source}.
+     * @throws NullPointerException If either {@code source} or {@code key} is {@code null}.
+     */
+    public JsonObject nullOrMissingJsonObject(final JsonObject source, final String key) throws NullPointerException {
+        Objects.requireNonNull(source);
+        Objects.requireNonNull(key);
+        return nullOrMissingValue(source, source::getJsonObject, key, null);
+    }
+
+    /**
+     * Gets a JSON array from a key that may be missing or {@code NULL}.
+     * @param source The source JSON object.
+     * @param key The key.
+     * @return A {@code JsonArray} instance, may be {@code null} if {@code key} is {@code NULL} or absent from {@code source}.
+     * @throws NullPointerException If either {@code source} or {@code key} is {@code null}.
+     */
+    public JsonArray nullOrMissingJsonArray(final JsonObject source, final String key) throws NullPointerException {
+        Objects.requireNonNull(source);
+        Objects.requireNonNull(key);
+        return nullOrMissingValue(source, source::getJsonArray, key, null);
+    }
+
+    /**
+     * Gets a JSON number from a key that may be missing or {@code NULL}.
+     * @param source The source JSON object.
+     * @param key The key.
+     * @return A {@code JsonNumber} instance, may be {@code null} if {@code key} is {@code NULL} or absent from {@code source}.
+     * @throws NullPointerException If either {@code source} or {@code key} is {@code null}.
+     */
+    public JsonNumber nullOrMissingJsonNumber(final JsonObject source, final String key) throws NullPointerException {
+        Objects.requireNonNull(source);
+        Objects.requireNonNull(key);
+        return nullOrMissingValue(source, source::getJsonNumber, key, null);
+    }
+
+    /**
+     * Gets a value from a key that may be missing or {@code NULL}.
+     * @param <T> The type to use.
+     * @param source The source JSON object.
+     * @param supplier The supplier function.
+     * @param key The key.
+     * @param defaultValue The default value.
+     * @return A {@code T} instance, may be {@code defaultValue} if {@code key} is {@code NULL} or absent from {@code source}.
+     * @throws NullPointerException If either {@code source}, {@code supplier} or {@code key} is {@code null}.
+     */
+    private static <T> T nullOrMissingValue(final JsonObject source, final Function<String, T> supplier, final String key, final T defaultValue) throws NullPointerException {
+        Objects.requireNonNull(source);
+        Objects.requireNonNull(supplier);
+        Objects.requireNonNull(key);
+        return (!source.containsKey(key) || source.isNull(key)) ? defaultValue : supplier.apply(key);
     }
 }
