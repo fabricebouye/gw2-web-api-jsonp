4a80cdc7478c87b26aec7203ac2c6e6aa4e665a5
Fabrice Bouyé <fabriceb@spc.int>
2015-12-04 14:20:42 +1100

    Added JSONP implementation of v2/build.

---
+++ src/api/web/gw2/mapping/v2/build/JsonpBuild.java
@@ -0,0 +1,30 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.build;
+
+import api.web.gw2.mapping.core.IdValue;
+
+/**
+ * JSONP implementation of a build.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpBuild implements Build {
+    @IdValue
+    private int id = -1;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpBuild() {
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+}
