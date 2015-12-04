2367360f5b66c764ca499342775479daaccce216
Fabrice Bouyé <fabriceb@spc.int>
2015-12-04 14:54:26 +1100

    Fixed class non final.

--- test/api/web/gw2/mapping/v2/account/JsonpAccountTest.java
+++ test/api/web/gw2/mapping/v2/account/JsonpAccountTest.java
@@ -23,7 +23,7 @@ import static org.junit.Assert.*;
  * Unit test.
  * @author Fabrice Bouyé
  */
-public class JsonpAccountTest {
+public final class JsonpAccountTest {
 
     public JsonpAccountTest() {
     }
