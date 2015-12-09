9459a7f6a3ff718f5284bb173ac3f007a251e331
Fabrice Bouyé <fabriceb@spc.int>
2015-12-09 15:08:01 +1100

    Added JSON-P implementation of v2/characters/inventory.

---
+++ src/api/web/gw2/mapping/v2/characters/inventory/JsonpInventory.java
@@ -0,0 +1,39 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.characters.inventory;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.QuantityValue;
+
+/**
+ * Default JSON-P implementation of an inventory item.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpInventory implements Inventory {
+
+    @IdValue
+    private int id = -1;
+    @QuantityValue
+    private short count = 0;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpInventory() {
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public short getCount() {
+        return count;
+    }
+}
---
+++ src/api/web/gw2/mapping/v2/characters/inventory/JsonpInventoryBag.java
@@ -0,0 +1,48 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.characters.inventory;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.ListValue;
+import api.web.gw2.mapping.core.QuantityValue;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Default JSON-P  implementation of a bag.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpInventoryBag implements InventoryBag {
+    @IdValue
+    private int id = -1;
+    @QuantityValue
+    private int size = 0;
+    @ListValue
+    private List<Inventory> inventory = Collections.EMPTY_LIST;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpInventoryBag() {
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public List<Inventory> getInventory() {
+        return inventory;
+    }
+}
