eb18b9ef2d969e14bbf960c50c45dff8510aa5b6
Fabrice Bouyé <fabriceb@spc.int>
2015-12-10 13:50:22 +1100

    Added JSONP implementation of v2/specializations.

---
+++ src/api/web/gw2/mapping/v2/specializations/JsonpSpecialization.java
@@ -0,0 +1,85 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.specializations;
+
+import api.web.gw2.mapping.core.EnumValue;
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.LocalizedResource;
+import api.web.gw2.mapping.core.OptionalValue;
+import api.web.gw2.mapping.core.SetValue;
+import api.web.gw2.mapping.core.URLValue;
+import api.web.gw2.mapping.v2.characters.CharacterProfession;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * Default JSON-P implementation of a specialization.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpSpecialization implements Specialization {
+
+    @IdValue
+    private int id = -1;
+    @LocalizedResource
+    private String name = ""; // NOI18N.
+    @OptionalValue
+    @EnumValue(factory = "api.web.gw2.mapping.v2.characters.CharactersUtils::findCharacterProfession") // NOI18N.
+    private CharacterProfession profession = CharacterProfession.UNKNOWN;
+    private boolean elite;
+    @OptionalValue
+    @URLValue
+    private Optional<URL> icon;
+    @SetValue
+    private Set<Integer> minorTraits = Collections.EMPTY_SET;
+    @SetValue
+    private Set<Integer> majorTraits = Collections.EMPTY_SET;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpSpecialization() {
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public CharacterProfession getProfession() {
+        return profession;
+    }
+
+    @Override
+    public boolean isElite() {
+        return elite;
+    }
+
+    @Override
+    public Optional<URL> getIcon() {
+        return icon;
+    }
+
+    @Override
+    public Set<Integer> getMinorTraits() {
+        return minorTraits;
+    }
+
+    @Override
+    public Set<Integer> getMajorTraits() {
+        return majorTraits;
+    }
+
+}
