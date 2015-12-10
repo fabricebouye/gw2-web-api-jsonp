8a39089b2e90548e13130896534b7fbc09dc304c
Fabrice Bouyé <fabrice.bouye@gmail.com>
2015-12-10 23:20:53 +1100

    Added preliminary JSON-P implementation of v2/traits/TraitPercentFact.

---
+++ src/api/web/gw2/mapping/v2/traits/JsonpTraitPercentFact.java
@@ -0,0 +1,31 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.traits;
+
+import api.web.gw2.mapping.core.PercentValue;
+
+/**
+ * Default JSON-P implementation of a trait percent fact.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpTraitPercentFact extends JsonpTraitFact implements TraitPercentFact {
+
+    @PercentValue
+    private int percent = 0;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpTraitPercentFact() {
+    }
+
+    @Override
+    public int getPercent() {
+        return percent;
+    }
+}
