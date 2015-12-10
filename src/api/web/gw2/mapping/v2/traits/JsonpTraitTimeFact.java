d2fe89121c01800b59587a7578dbc8807ea5057b
Fabrice Bouyé <fabrice.bouye@gmail.com>
2015-12-10 23:26:32 +1100

    Added preliminary JSON-P implementation of v2/traits/TraitTimeFact.

---
+++ src/api/web/gw2/mapping/v2/traits/JsonpTraitTimeFact.java
@@ -0,0 +1,32 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.traits;
+
+import api.web.gw2.mapping.core.DurationValue;
+import java.time.Duration;
+
+/**
+ * Default JSON-P implementation of a trait time fact.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpTraitTimeFact extends JsonpTraitFact implements TraitTimeFact {
+    @DurationValue
+    private Duration duration = Duration.ZERO;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpTraitTimeFact() {
+    }
+
+    @Override
+    public Duration getDuration() {
+        return duration;
+    }
+
+}
