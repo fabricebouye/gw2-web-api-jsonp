230ef913e0e8349974518046447fa3230858fe2d
Fabrice Bouyé <fabriceb@spc.int>
2015-12-11 11:35:30 +1100

    Added preliminary JSON-P implementation of v2/traits/TraitComboFinisherFact.

---
+++ src/api/web/gw2/mapping/v2/traits/JsonpTraitComboFinisherFact.java
@@ -0,0 +1,39 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.traits;
+
+import api.web.gw2.mapping.core.EnumValue;
+import api.web.gw2.mapping.core.PercentValue;
+
+/**
+ * Default JSON-P implementation of a trait combo finisher fact.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpTraitComboFinisherFact extends JsonpTraitFact implements TraitComboFinisherFact {
+
+    @PercentValue
+    private int percent = 0;
+    @EnumValue(factory = "api.web.gw2.mapping.v2.traits.TraitsUtils::findTraitComboFinisherType") // NOI18N.
+    private TraitComboFinisherType finisherType = TraitComboFinisherType.UNKNOWN;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpTraitComboFinisherFact() {
+    }
+
+    @Override
+    public int getPercent() {
+        return percent;
+    }
+
+    @Override
+    public TraitComboFinisherType getFinisherType() {
+        return finisherType;
+    }
+}
