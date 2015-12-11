2aa65da2306950adf4e8f1916baf6e1577208094
Fabrice Bouyé <fabriceb@spc.int>
2015-12-11 11:12:36 +1100

    Added preliminary JSON-P implementation of v2/traits/TraitNumberFact.

---
+++ src/api/web/gw2/mapping/v2/traits/JsonpTraitNumberFact.java
@@ -0,0 +1,30 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.traits;
+
+import api.web.gw2.mapping.core.QuantityValue;
+
+/**
+ * Default JSON-P implementation of a trait number fact.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpTraitNumberFact extends JsonpTraitFact implements TraitNumberFact {
+    @QuantityValue
+    private int value;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpTraitNumberFact() {
+    }
+
+    @Override
+    public int getValue() {
+        return value;
+    }
+}
