df0878524661dd9c6240c0fe994121bf733e1c30
Fabrice Bouyé <fabriceb@spc.int>
2015-12-11 11:35:08 +1100

    Added preliminary JSON-P implementation of v2/traits/TraitComboFieldFact.

---
+++ src/api/web/gw2/mapping/v2/traits/JsonpTraitComboFieldFact.java
@@ -0,0 +1,31 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.traits;
+
+import api.web.gw2.mapping.core.EnumValue;
+
+/**
+ * Default JSON-P implementation of a trait combo field fact.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpTraitComboFieldFact extends JsonpTraitFact implements TraitComboFieldFact {
+
+    @EnumValue(factory = "api.web.gw2.mapping.v2.traits.TraitsUtils::findTraitComboFieldType") // NOI18N.
+    private TraitComboFieldType fieldType = TraitComboFieldType.UNKNOWN;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpTraitComboFieldFact() {
+    }
+
+    @Override
+    public TraitComboFieldType getFieldType() {
+        return fieldType;
+    }
+}
