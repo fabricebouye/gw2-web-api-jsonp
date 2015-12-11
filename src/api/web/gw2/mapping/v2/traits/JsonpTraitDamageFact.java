fb616d9b28cf3a0002eb2bacc389d9196743d279
Fabrice Bouyé <fabriceb@spc.int>
2015-12-11 11:35:44 +1100

    Added preliminary JSON-P implementation of v2/traits/TraitDamageFact.

---
+++ src/api/web/gw2/mapping/v2/traits/JsonpTraitDamageFact.java
@@ -0,0 +1,31 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.traits;
+
+import api.web.gw2.mapping.core.QuantityValue;
+
+/**
+ * Default JSON-P implementation of a trait damage fact.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpTraitDamageFact extends JsonpTraitFact implements TraitDamageFact {
+
+    @QuantityValue
+    private int hitCount = 0;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpTraitDamageFact() {
+    }
+
+    @Override
+    public int getHitCount() {
+        return hitCount;
+    }
+}
