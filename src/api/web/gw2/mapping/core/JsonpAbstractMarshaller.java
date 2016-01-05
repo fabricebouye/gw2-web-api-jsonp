fd728447882ecc6c1bdf857478438ce89f0a49fc
Fabrice Bouyé <fabriceb@spc.int>
2016-01-05 15:44:20 +1100

    Fixed imports.

--- src/api/web/gw2/mapping/core/JsonpAbstractMarshaller.java
+++ src/api/web/gw2/mapping/core/JsonpAbstractMarshaller.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.net.MalformedURLException;
 import java.net.URL;
