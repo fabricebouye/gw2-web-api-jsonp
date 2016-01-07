b55524ea97022bd34724c7b8e0b42160f1b62d83
Fabrice Bouyé <fabriceb@spc.int>
2016-01-08 08:53:48 +1100

    Added missing types and services.

---
+++ src/api/web/gw2/mapping/v2/items/JsonpItemContainerDetails.java
@@ -0,0 +1,26 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.items;
+
+/**
+ * Default JSON-P implementation of a container details.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpItemContainerDetails extends JsonpItemDetails implements ItemContainerDetails {
+
+    private ItemContainerType type = ItemContainerType.UNKNOWN;
+
+    public JsonpItemContainerDetails() {
+        super(ItemType.BAG);
+    }
+
+    @Override
+    public ItemContainerType getType() {
+        return type;
+    }
+}
