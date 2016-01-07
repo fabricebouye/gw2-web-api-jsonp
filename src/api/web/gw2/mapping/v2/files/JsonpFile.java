b5c34317b3a9eb8bdc44a3e10d0c3d325bab7310
Fabrice Bouyé <fabriceb@spc.int>
2016-01-08 10:28:55 +1100

    Added preliminary JSON-P implementation of v2/files.

---
+++ src/api/web/gw2/mapping/v2/files/JsonpFile.java
@@ -0,0 +1,43 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.files;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.OptionalValue;
+import api.web.gw2.mapping.core.URLValue;
+import java.net.URL;
+import java.util.Optional;
+
+/**
+ * Default JSON-P implementation of a file.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpFile implements File {
+
+    @IdValue(flavor = IdValue.Flavor.STRING)
+    private String id = "";
+    @OptionalValue
+    @URLValue
+    Optional<URL> icon = Optional.empty();
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpFile() {
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public Optional<URL> getIcon() {
+        return icon;
+    }
+}
