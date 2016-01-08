10e9b19fe361657953934322365a929c76933ce7
Fabrice Bouyé <fabriceb@spc.int>
2016-01-08 15:51:32 +1100

    Added preliminary JSON-P implementation for v2/commerce/prices.

---
+++ src/api/web/gw2/mapping/v2/commerce/prices/JsonpPrice.java
@@ -0,0 +1,48 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.commerce.prices;
+
+import api.web.gw2.mapping.core.IdValue;
+
+/**
+ * Default JSON-P implementation of a price.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpPrice implements Price {
+    @IdValue
+    private int id = -1;    
+    private PriceOrder buys = PriceOrder.EMPTY;
+    private PriceOrder sells = PriceOrder.EMPTY;
+    private boolean whitelisted = false;
+
+    /**
+    * Creates a new empty instance.
+    */
+    public JsonpPrice() {        
+    }
+    
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public PriceOrder getBuys() {
+        return buys;
+    }
+
+    @Override
+    public PriceOrder getSells() {
+        return sells;
+    }
+
+    @Override
+    public boolean isWhitelisted() {
+        return whitelisted;
+    }
+}
---
+++ src/api/web/gw2/mapping/v2/commerce/prices/JsonpPriceOrder.java
@@ -0,0 +1,40 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.commerce.prices;
+
+import api.web.gw2.mapping.core.CoinAmount;
+import api.web.gw2.mapping.core.CoinValue;
+import api.web.gw2.mapping.core.QuantityValue;
+
+/**
+ * Default JSON-P implementation of an order.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpPriceOrder implements PriceOrder {
+
+    @CoinValue
+    private CoinAmount unitPrice = CoinAmount.ZERO;
+    @QuantityValue
+    private int quantity = 0;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpPriceOrder() {
+    }
+
+    @Override
+    public CoinAmount getUnitPrice() {
+        return unitPrice;
+    }
+
+    @Override
+    public int getQuantity() {
+        return quantity;
+    }
+}
