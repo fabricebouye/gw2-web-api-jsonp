92716820047d466e5f3a576c52e12052ccb770f7
Fabrice Bouyé <fabriceb@spc.int>
2016-01-08 15:51:11 +1100

    Added preliminary JSON-P implementation for v2/commerce/listings.

---
+++ src/api/web/gw2/mapping/v2/commerce/listings/JsonpListingResponse.java
@@ -0,0 +1,48 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.commerce.listings;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.SetValue;
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * Default JSON-P implementation of a listing response.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpListingResponse implements ListingResponse {
+
+    @IdValue
+    private int id = -1;
+    @SetValue
+    private Set<Listing> buys = Collections.EMPTY_SET;
+    @SetValue
+    private Set<Listing> sells = Collections.EMPTY_SET;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpListingResponse() {
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public Set<Listing> getBuys() {
+        return buys;
+    }
+
+    @Override
+    public Set<Listing> getSells() {
+        return sells;
+    }
+}
---
+++ src/api/web/gw2/mapping/v2/commerce/listings/JsonptListing.java
@@ -0,0 +1,47 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.commerce.listings;
+
+import api.web.gw2.mapping.core.CoinAmount;
+import api.web.gw2.mapping.core.CoinValue;
+import api.web.gw2.mapping.core.QuantityValue;
+
+/**
+ * Default JSON-P implementation of a listing
+ * @author Fabrice Bouyé
+ */
+public final class JsonptListing implements Listing {
+
+    @QuantityValue
+    private int listing = 0;
+    @CoinValue
+    private CoinAmount unitPrice = CoinAmount.ZERO;
+    @QuantityValue
+    private int quantity = 0;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonptListing() {
+    }
+
+    @Override
+    public int getListings() {
+        return listing;
+    }
+
+    @Override
+    public CoinAmount getUnitPrice() {
+        return unitPrice;
+    }
+
+    @Override
+    public int getQuantity() {
+        return quantity;
+    }
+}
