ff937ad4af13ee4e1be0c6a307f8c5a84368a774
Fabrice Bouyé <fabriceb@spc.int>
2016-01-08 15:50:45 +1100

    Added preliminary JSON-P implementation for v2/commerce/exchange.

---
+++ src/api/web/gw2/mapping/v2/commerce/exchange/JsonpExchangeResponse.java
@@ -0,0 +1,33 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.commerce.exchange;
+
+import api.web.gw2.mapping.core.SetValue;
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * Default JSON-P implementation of the exchange result
+ * @author Fabrice Bouyé
+ */
+public final class JsonpExchangeResponse implements ExchangeResponse {
+
+    @SetValue
+    private Set<ExchangeResource> resources = Collections.EMPTY_SET;
+
+    /**
+     * Creates a new empty result.
+     */
+    public JsonpExchangeResponse() {
+    }
+
+    @Override
+    public Set<ExchangeResource> getResources() {
+        return resources;
+    }
+}
---
+++ src/api/web/gw2/mapping/v2/commerce/exchange/JsonptExchangeRate.java
@@ -0,0 +1,40 @@
+/* 
+ * Copyright (C) 2015 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.commerce.exchange;
+
+import api.web.gw2.mapping.core.CoinAmount;
+import api.web.gw2.mapping.core.CoinValue;
+import api.web.gw2.mapping.core.QuantityValue;
+
+/**
+ * Default JSON-P implementation of an exchange rate.
+ * @author Fabrice Bouyé
+ */
+public final class JsonptExchangeRate implements ExchangeRate {
+
+    @CoinValue
+    private CoinAmount coinsPerGem = CoinAmount.ZERO;
+    @QuantityValue
+    private int quantity = 0;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonptExchangeRate() {
+    }
+
+    @Override
+    public CoinAmount getCoinsPerGem() {
+        return coinsPerGem;
+    }
+
+    @Override
+    public int getQuantity() {
+        return quantity;
+    }
+}
