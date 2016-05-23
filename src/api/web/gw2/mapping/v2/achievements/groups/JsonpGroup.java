675a4c7e925d4644b9f60ed0c9530f441fc12689
Fabrice Bouyé <fabriceb@spc.int>
2016-05-23 11:36:48 +1100

    Added JSON-P implementation of v2/achievements/groups.

---
+++ src/api/web/gw2/mapping/v2/achievements/groups/JsonpGroup.java
@@ -0,0 +1,64 @@
+/* 
+ * Copyright (C) 2015-2016 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.achievements.groups;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.LocalizedResource;
+import api.web.gw2.mapping.core.QuantityValue;
+import api.web.gw2.mapping.core.SetValue;
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * Default JSON-P implementation of an achievement category group.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpGroup implements Group {
+
+    @IdValue(flavor = IdValue.Flavor.STRING)
+    private String id = IdValue.DEFAULT_STRING_ID;
+    @LocalizedResource
+    private String name = LocalizedResource.DEFAULT;
+    @LocalizedResource
+    private String description = LocalizedResource.DEFAULT;
+    @QuantityValue
+    private int order = QuantityValue.DEFAULT;
+    @SetValue
+    private Set<Integer> categories = Collections.EMPTY_SET;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpGroup() {
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public int getOrder() {
+        return order;
+    }
+
+    @Override
+    public Set<Integer> getCategories() {
+        return categories;
+    }
+}
