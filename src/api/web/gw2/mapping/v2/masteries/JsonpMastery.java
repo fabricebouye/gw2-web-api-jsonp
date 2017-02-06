20d653629ca5567da2610cea16ed7369ff0284c2
Fabrice Bouyé <fabriceb@spc.int>
2017-02-06 12:10:48 +1100

    Added JSON-P implementation of v2/masteries.

---
+++ src/api/web/gw2/mapping/v2/masteries/JsonpMastery.java
@@ -0,0 +1,80 @@
+/* 
+ * Copyright (C) 2015-2017 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.masteries;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.LocalizedResource;
+import api.web.gw2.mapping.core.QuantityValue;
+import api.web.gw2.mapping.core.SetValue;
+import api.web.gw2.mapping.core.URLReference;
+import api.web.gw2.mapping.core.URLValue;
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * Default JSON-P implementation of a mastery.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpMastery implements Mastery {
+
+    @IdValue
+    private int id = IdValue.DEFAULT_INTEGER_ID;
+    @LocalizedResource
+    private String name = LocalizedResource.DEFAULT;
+    @LocalizedResource
+    private String requirements = LocalizedResource.DEFAULT;
+    @QuantityValue
+    private int order = QuantityValue.DEFAULT;
+    @URLValue
+    private URLReference background = URLReference.empty();
+    @IdValue(flavor = IdValue.Flavor.STRING)
+    private String region = IdValue.DEFAULT_STRING_ID;
+    @SetValue
+    private Set<MasteryLevel> levels = Collections.EMPTY_SET;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpMastery() {
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getRequirement() {
+        return requirements;
+    }
+
+    @Override
+    public int getOrder() {
+        return order;
+    }
+
+    @Override
+    public URLReference getBackground() {
+        return background;
+    }
+
+    @Override
+    public String getRegion() {
+        return region;
+    }
+
+    @Override
+    public Set<MasteryLevel> getLevels() {
+        return levels;
+    }
+}
