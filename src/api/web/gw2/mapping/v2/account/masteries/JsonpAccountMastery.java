391b5d84506417bd7ac860a624a3d9f858836b10
Fabrice Bouyé <fabriceb@spc.int>
2017-02-06 12:14:10 +1100

    Added JSON-P implementation of v2/account/masteries.

---
+++ src/api/web/gw2/mapping/v2/account/masteries/JsonpAccountMastery.java
@@ -0,0 +1,39 @@
+/* 
+ * Copyright (C) 2015-2017 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.mapping.v2.account.masteries;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.QuantityValue;
+
+/**
+ * Default JSON-P implementation of an account mastery unlock.
+ * @author Fabrice Bouyé
+ */
+public final class JsonpAccountMastery implements AccountMastery {
+
+    @IdValue
+    private int id = IdValue.DEFAULT_INTEGER_ID;
+    @QuantityValue
+    private int level = QuantityValue.DEFAULT;
+
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpAccountMastery() {
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public int getLevel() {
+        return level;
+    }
+}
