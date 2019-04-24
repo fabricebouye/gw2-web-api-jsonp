ebc71a7359710617001eb477b5d3bfe3ab784e5e
fabricebouye <fabrice.bouye@gmail.com>
2019-04-24 21:41:48 +1100

    Fixed annotation.

--- src/api/web/gw2/jsonp/v2/home/cats/JsonpHomeCat.java
+++ src/api/web/gw2/jsonp/v2/home/cats/JsonpHomeCat.java
@@ -19,8 +19,9 @@ import api.web.gw2.mapping.v2.home.cats.HomeCat;
 public final class JsonpHomeCat implements HomeCat {
     @IdValue
     private int id = IdValue.DEFAULT_INTEGER_ID;
-    private String hint = LocalizedResource.DEFAULT;
-    
+    @IdValue(flavor = IdValue.Flavor.STRING)
+    private String hint = IdValue.DEFAULT_STRING_ID;
+
     /**
      * Creates a new empty instance.
      */
