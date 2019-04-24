22c438ac03ba6b3b5bd445f8e68853b745128c6a
fabricebouye <fabrice.bouye@gmail.com>
2019-04-24 21:26:08 +1100

    Added JSON-P implementation of v2/home/cats.

---
+++ src/api/web/gw2/jsonp/v2/home/cats/JsonpHomeCat.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2015-2019 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+package api.web.gw2.jsonp.v2.home.cats;
+
+import api.web.gw2.mapping.core.IdValue;
+import api.web.gw2.mapping.core.LocalizedResource;
+import api.web.gw2.mapping.v2.home.cats.HomeCat;
+
+/**
+ * Default JSON-P implementation of a home cat.
+ *
+ * @author Fabrice Bouyé
+ */
+public final class JsonpHomeCat implements HomeCat {
+    @IdValue
+    private int id = IdValue.DEFAULT_INTEGER_ID;
+    private String hint = LocalizedResource.DEFAULT;
+    
+    /**
+     * Creates a new empty instance.
+     */
+    public JsonpHomeCat() {
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getHint() {
+        return hint;
+    }
+}
