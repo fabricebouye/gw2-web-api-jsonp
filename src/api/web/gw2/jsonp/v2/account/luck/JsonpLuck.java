b4fb2c501e5aab20dc15721f5abafce46ac35231
fabricebouye <fabrice.bouye@gmail.com>
2019-04-24 21:06:55 +1100

    Added missing copyright header.

--- src/api/web/gw2/jsonp/v2/account/luck/JsonpLuck.java
+++ src/api/web/gw2/jsonp/v2/account/luck/JsonpLuck.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2015-2019 Fabrice Bouyé
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
 package api.web.gw2.jsonp.v2.account.luck;
 
 import api.web.gw2.mapping.core.EnumValue;
