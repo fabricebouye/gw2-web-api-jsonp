0dcb81a00ee5b80c90982e1a280a10c81aecb796
Fabrice Bouye <fabriceb@spc.int>
2019-04-25 13:12:31 +1100

    Suppressing deprecation warning.

--- test/api/web/gw2/jsonp/v2/quaggans/JsonQuaggansUtilsTest.java
+++ test/api/web/gw2/jsonp/v2/quaggans/JsonQuaggansUtilsTest.java
@@ -24,6 +24,7 @@ import javax.json.stream.JsonParser.Event;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.util.Optional;
 
@@ -64,67 +65,64 @@ public class JsonQuaggansUtilsTest {
     }
 
     @Test
-    public void stream() throws IOException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+    public void stream() throws IOException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
         final URL url = getClass().getResource("quaggan1.json"); // NOI18N.
-        try (final InputStream source = url.openStream()) {
-            final JsonParser parser = Json.createParser(source);
+        try (final InputStream source = url.openStream();
+             final JsonParser parser = Json.createParser(source)) {
             Class aClass = api.web.gw2.jsonp.v2.quaggans.JsonpQuaggan.class;
-            Object instance = aClass.newInstance();
-            try {
-                String lastId = null;
-                while (parser.hasNext()) {
-                    final Event event = parser.next();
-                    switch (event) {
-                        case START_ARRAY:
-                            System.out.println("START_ARRAY");
-                            break;
-                        case START_OBJECT:
-                            System.out.println("START_OBJECT");
-                            break;
-                        case KEY_NAME:
-                            System.out.println("KEY_NAME");
-                            lastId = parser.getString();
-                            break;
-                        case VALUE_STRING:
-                            System.out.println("VALUE_STRING");
-                            final String string = parser.getString();
-                            final Field field = aClass.getDeclaredField(lastId);
-                            boolean isOptional = field.getAnnotation(OptionalValue.class) != null;
-                            boolean isURL = field.getAnnotation(URLValue.class) != null;
-                            boolean wasAcessible = field.isAccessible();
-                            field.setAccessible(true);
-                            Object value = string;
-                            if (isURL) {
-                                value = URLReference.of(string);
-                            }
-                            if (isOptional) {
-                                value = Optional.of(value);
-                            }
-                            field.set(instance, value);
-                            field.setAccessible(wasAcessible);
-                            break;
-                        case VALUE_NUMBER:
-                            System.out.println("VALUE_NUMBER");
-                            break;
-                        case VALUE_TRUE:
-                            System.out.println("VALUE_TRUE");
-                            break;
-                        case VALUE_FALSE:
-                            System.out.println("VALUE_FALSE");
-                            break;
-                        case VALUE_NULL:
-                            System.out.println("VALUE_NULLz");
-                            break;
-                        case END_OBJECT:
-                            System.out.println("END_OBJECT");
-                            break;
-                        case END_ARRAY:
-                            System.out.println("END_ARRAY");
-                            break;
-                    }
+            Object instance = aClass.getDeclaredConstructor()
+                    .newInstance();
+            String lastId = null;
+            while (parser.hasNext()) {
+                final Event event = parser.next();
+                switch (event) {
+                    case START_ARRAY:
+                        System.out.println("START_ARRAY");
+                        break;
+                    case START_OBJECT:
+                        System.out.println("START_OBJECT");
+                        break;
+                    case KEY_NAME:
+                        System.out.println("KEY_NAME");
+                        lastId = parser.getString();
+                        break;
+                    case VALUE_STRING:
+                        System.out.println("VALUE_STRING");
+                        final String string = parser.getString();
+                        final Field field = aClass.getDeclaredField(lastId);
+                        boolean isOptional = field.getAnnotation(OptionalValue.class) != null;
+                        boolean isURL = field.getAnnotation(URLValue.class) != null;
+                        boolean wasAcessible = field.canAccess(instance);
+                        field.setAccessible(true);
+                        Object value = string;
+                        if (isURL) {
+                            value = URLReference.of(string);
+                        }
+                        if (isOptional) {
+                            value = Optional.of(value);
+                        }
+                        field.set(instance, value);
+                        field.setAccessible(wasAcessible);
+                        break;
+                    case VALUE_NUMBER:
+                        System.out.println("VALUE_NUMBER");
+                        break;
+                    case VALUE_TRUE:
+                        System.out.println("VALUE_TRUE");
+                        break;
+                    case VALUE_FALSE:
+                        System.out.println("VALUE_FALSE");
+                        break;
+                    case VALUE_NULL:
+                        System.out.println("VALUE_NULLz");
+                        break;
+                    case END_OBJECT:
+                        System.out.println("END_OBJECT");
+                        break;
+                    case END_ARRAY:
+                        System.out.println("END_ARRAY");
+                        break;
                 }
-            } finally {
-                parser.close();
             }
         }
     }
